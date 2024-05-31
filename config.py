765789eb5c29cc2b60bc282f093200a0bab58cb7
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-05-31 16:22:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,9 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b><i>📁Filename:{filename}
+
+💡After Downloading Click on The File And Open It Using The External Video Player. Report This File <a href="https://t.me/AsianpurAdminBot">@AsianpurAdminBot</a></i></b> ")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
