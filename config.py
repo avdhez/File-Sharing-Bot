9972352e4fa3a287757011c534d4ec780c02825d
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-08-11 09:56:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,60 +1,54 @@
-#(©)CodeXBotz
-
-
-
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
+# Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7419533572:AAEqB6SY-HKDLc4xBW2IgbfL5YHit4y2SZA")
 
+# Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "6989623"))
 
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "077c18ef264de1c09a32ee2d5c00c79b")
 
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+# Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001830949470"))
 
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+# OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "5788821671"))
 
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-
-#Port
+# Port
 PORT = os.environ.get("PORT", "8080")
 
-#Database 
+# Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kkvjhx15ef:Nq5qDQpj1XpHeCjY@cluster0.izercbs.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+# Force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n I can store private files in Specified Channel and other users can access it from special link.")
+# Start message
+START_MSG = os.environ.get("START_MESSAGE", "<b><i>Hello {first}\nI can store private files in Specified Channel and other users can access it from special link.</i></b>")
+
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5143516512").split()):
+    ADMINS = []
+    for x in (os.environ.get("ADMINS", "5788821671").split()):
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+# Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b><i>Hello {first}\n<b>You need to join in my Channel/Group to use me\nKindly Please join Channel</i></b>")
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION","<b><i>Filename: {filename}\n\nJust Download this file and open it by clicking on it and choosing right option to play the videos or installing apks and zip file\n\nif found any difficulty then contact the admin</i></b>")
+# Set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b><i>Filename: {filename}\n\nJust Download this file and open it by clicking on it and choosing right option to play the videos or installing apks and zip file\n\nif found any difficulty then contact the admin</i></b>")
 
-#set True if you want to prevent users from forwarding files from bot
+# Set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
-#Set true if you want Disable your Channel Posts Share button
+# Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
@@ -79,7 +73,6 @@ logging.basicConfig(
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
