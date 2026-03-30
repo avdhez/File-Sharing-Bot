bbd7bb9310448742c0df593e4b1de6577f10c458
Avdhez <118711523+avdhez@users.noreply.github.com>
2026-03-30 11:15:43 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,40 +1,45 @@
 import asyncio
+import base64
+
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, SAFELINK_WEBSITE
 from helper_func import encode
 
 
+def make_safelink(telegram_link: str) -> str:
+    """Wrap a Telegram bot link with the safelink page (replicates JS btoa())."""
+    encoded = base64.b64encode(telegram_link.encode("utf-8")).decode("utf-8")
+    return f"{SAFELINK_WEBSITE}?url={encoded}"
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
         await reply_text.edit_text("Something Went Wrong..!")
         return
+
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    tg_link = f"https://t.me/{client.username}?start={base64_string}"
+    safe_link = make_safelink(tg_link)
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={safe_link}')]])
 
-    await reply_text.edit(f"<b>Here Is Your Link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Here Is Your Link</b>\n\n{safe_link}", reply_markup=reply_markup, disable_web_page_preview=True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
-
-
-
 
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
@@ -46,8 +51,9 @@ async def new_post(client: Client, message: Message):
     converted_id = message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    tg_link = f"https://t.me/{client.username}?start={base64_string}"
+    safe_link = make_safelink(tg_link)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={safe_link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
@@ -55,11 +61,7 @@ async def new_post(client: Client, message: Message):
         pass
 
 
-
-
-
-
-# Jishu Developer 
+# Jishu Developer
 # Don't Remove Credit 🥺
 # Telegram Channel @Madflix_Bots
 # Backup Channel @JishuBotz
